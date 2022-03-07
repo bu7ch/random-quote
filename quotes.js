38fc78bc490847b02203f4ef92564bef19aba73a
Rodolphe <dutchr@live.fr>
2022-03-07 15:05:41 +0100

    :sparkles:update authors

--- quotes.js
+++ quotes.js
@@ -4,13 +4,13 @@ const randomTag = document.querySelector("footer img");
 
 const data = [
 
-  {quote:"La motivation vous sert de départ. L'habitude vous fait continuer.", author:"" },
-  {quote:"Ce n’est pas parce que c’est difficile qu’on n’ose pas le faire, mais parce qu’on n’ose pas le faire que c’est difficile.", author:""},
-  {quote:"Beaucoup d’hommes ayant échoué ne savaient pas à quel point ils étaient proches du succès quand ils ont abandonné", author:""},
-  {quote:"Le talent ne suffit pas. A part quelques rares exceptions, les meilleurs joueurs sont les plus gros travailleurs.", author:""},
-  {quote:"J’attribue mon succès à ceci : je n’ai jamais présenté ou accepté aucune excuse.", author:""},
-  {quote:"Croyez en vos rêves et ils se réaliseront peut-être. Croyez en vous et ils se réaliseront sûrement.", author:""},
-  {quote:"Faites ce que vous pouvez, là où vous êtes, avec ce que vous avez", author:""},
+  {quote:"La motivation vous sert de départ. L'habitude vous fait continuer.", author:"Jim Ryun" },
+  {quote:"Ce n’est pas parce que c’est difficile qu’on n’ose pas le faire, mais parce qu’on n’ose pas le faire que c’est difficile.", author:"Sénèque"},
+  {quote:"Beaucoup d’hommes ayant échoué ne savaient pas à quel point ils étaient proches du succès quand ils ont abandonné", author:"Thomas Edison"},
+  {quote:"Le talent ne suffit pas. A part quelques rares exceptions, les meilleurs joueurs sont les plus gros travailleurs.", author:"Magic johnson"},
+  {quote:"J’attribue mon succès à ceci : je n’ai jamais présenté ou accepté aucune excuse.", author:"Earl Nightingale"},
+  {quote:"Croyez en vos rêves et ils se réaliseront peut-être. Croyez en vous et ils se réaliseront sûrement.", author:"Martin Luther King"},
+  {quote:"Faites ce que vous pouvez, là où vous êtes, avec ce que vous avez", author:"Teddy Roosevelt "},
 ];
 const getQuote = function(){
   // get a random quote
